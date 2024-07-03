2875431495f9726bcaeac7476a28cf517d18d4a9
Juan Camilo Cuenca <camilocuenc1810@gmail.com>
2024-07-03 17:35:01 -0500

    feat(Role): create Role entity, controller, repository, and services

---
+++ src/main/java/com/library/library_management/entity/Role.java
@@ -0,0 +1,15 @@
+package com.library.library_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+    private String roleName;
+
+}
